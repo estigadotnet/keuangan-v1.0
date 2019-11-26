b90b0c20aa285434b3e2870881d2a8a027f25000
estigadotnet@lenovo <project@estiga.net>
2019-11-26 20:10:05 +0700

    .

--- classes/Api.php
+++ classes/Api.php
@@ -8,7 +8,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
  */
 class Api
 {
-	protected $SecretKey = 'tjcP1r1tZkIdiede';
+	protected $SecretKey = 'v9KtmCycFJ4od9Wt';
 	protected $Algorithm = 'HS512';
 
 	// For some reason, the "Authorization" header is removed by IIS, changed to "X-Authorization"
